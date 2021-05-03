9d08845002e726079da1b0917b5c85abcf23db5e
Antonino Verde <antoninoverde@live.com>
2021-05-04 00:08:49 +0200

    mapping's business logic review

--- src/main/java/com/opymi/otamap/util/OTABuilder.java
+++ src/main/java/com/opymi/otamap/util/OTABuilder.java
@@ -26,13 +26,14 @@ package com.opymi.otamap.util;
 
 import com.opymi.otamap.exception.AccessPropertyException;
 import com.opymi.otamap.exception.CreateInstanceException;
-import com.opymi.otamap.internal.OTMapperFactory;
-import com.opymi.otamap.internal.OTMapperRepository;
+import com.opymi.otamap.exception.OTException;
 
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.logging.Logger;
 
 /**
  * Class that builds a {@param <TARGET>} object from an {@param <ORIGIN>} object
@@ -41,26 +42,37 @@ import java.util.Objects;
  * @since 1.0
  */
 public class OTABuilder<ORIGIN, TARGET> {
+    private static final Map<Class<?>, Class<?>> SIMPLE_WRAPPER_TYPES;
+    static {
+        SIMPLE_WRAPPER_TYPES = new HashMap<>();
+        SIMPLE_WRAPPER_TYPES.put(Boolean.class, boolean.class);
+        SIMPLE_WRAPPER_TYPES.put(Integer.class, int.class);
+        SIMPLE_WRAPPER_TYPES.put(Double.class, double.class);
+        SIMPLE_WRAPPER_TYPES.put(Float.class, float.class);
+        SIMPLE_WRAPPER_TYPES.put(Long.class, long.class);
+        SIMPLE_WRAPPER_TYPES.put(Short.class, short.class);
+        SIMPLE_WRAPPER_TYPES.put(Character.class, char.class);
+        SIMPLE_WRAPPER_TYPES.put(Byte.class, byte.class);
+    }
 
-    private static final Map<Class<?>, Class<?>> SIMPLE_WRAPPER_TYPES = Map.of(
-            Boolean.class, boolean.class,
-            Integer.class, int.class,
-            Double.class, double.class,
-            Float.class, float.class,
-            Long.class, long.class,
-            Short.class, short.class,
-            Character.class, char.class,
-            Byte.class, byte.class
-    );
-
+    private static final Logger logger = Logger.getLogger(OTABuilder.class.getSimpleName());
     private final OTMapper<ORIGIN, TARGET> mapper;
+    private final OTMapperRepository repository;
+    private final OTMapperFactory factory;
 
-    private OTABuilder(OTMapper<ORIGIN, TARGET> mapper) {
+    private OTABuilder(OTMapperRepository repository, OTMapper<ORIGIN, TARGET> mapper) {
+        this.repository = repository != null ? repository : new OTMapperRepository();
+        if (!repository.exists(mapper.getOriginClass(), mapper.getTargetClass())) {
+            repository.store(mapper);
+        }
+        this.factory = new OTMapperFactory(repository);
         this.mapper = mapper;
     }
 
-    private OTABuilder(Class<ORIGIN> origin, Class<TARGET> target) {
-        this(OTMapperFactory.mapperForClasses(origin, target));
+    private OTABuilder(OTMapperRepository repository, Class<ORIGIN> origin, Class<TARGET> target) {
+        this.repository = repository;
+        this.factory = new OTMapperFactory(repository);
+        this.mapper = factory.mapperForClasses(origin, target);
     }
 
     /**
@@ -71,8 +83,15 @@ public class OTABuilder<ORIGIN, TARGET> {
      * @param <TARGET> target object
      * @return {@link OTABuilder} instance
      */
+    public static <ORIGIN, TARGET> OTABuilder<ORIGIN, TARGET> instance(OTMapperRepository repository, OTMapper<ORIGIN, TARGET> mapper) {
+        return new OTABuilder<>(repository, mapper);
+    }
+
+    /**
+     * @see #instance(OTMapperRepository, OTMapper)
+     */
     public static <ORIGIN, TARGET> OTABuilder<ORIGIN, TARGET> instance(OTMapper<ORIGIN, TARGET> mapper) {
-        return new OTABuilder<>(mapper);
+        return instance(null, mapper);
     }
 
     /**
@@ -82,8 +101,15 @@ public class OTABuilder<ORIGIN, TARGET> {
      * @param <TARGET> target type
      * @return {@link OTABuilder} instance
      */
+    public static <ORIGIN, TARGET> OTABuilder<ORIGIN, TARGET> instance(OTMapperRepository repository, Class<ORIGIN> origin, Class<TARGET> target) {
+        return new OTABuilder<>(repository, origin, target);
+    }
+
+    /**
+     * @see #instance(OTMapperRepository, Class, Class)
+     */
     public static <ORIGIN, TARGET> OTABuilder<ORIGIN, TARGET> instance(Class<ORIGIN> origin, Class<TARGET> target) {
-        return new OTABuilder<>(origin, target);
+        return instance(null, origin, target);
     }
 
     /**
@@ -103,15 +129,25 @@ public class OTABuilder<ORIGIN, TARGET> {
      * @throws CreateInstanceException if this method is trying to create an instance of target but
      * default constructor doesn't exists for the type
      */
+    @SuppressWarnings("unchecked")
     public final TARGET build(ORIGIN origin, TARGET target, boolean deepAutomatedBuild) {
+        if (origin == null) {
+            return null;
+        }
+
         final TARGET newTarget = target != null ? target : createInstance(mapper.getTargetClass());
 
-        mapper.getMappedProperties().forEach((originProperty, targetProperty) -> {
+        Class<?> originClass = origin.getClass();
+        Class<?> targetClass = newTarget.getClass();
+        if (Objects.equals(originClass, targetClass) || primitivable(originClass, targetClass)) {
+            return (TARGET) origin;
+        }
+
+        verifyAndGetMapping(mapper).forEach((originProperty, targetProperty) -> {
             try {
                 writeProperty(origin, originProperty, newTarget, targetProperty, deepAutomatedBuild);
             } catch (IllegalAccessException | InvocationTargetException cause) {
-                String message = "CANNOT READ ORIGIN'S PROPERTY " + originProperty.getName()
-                        + " OR CANNOT WRITE TARGET'S PROPERTY " + targetProperty.getName();
+                String message = "CANNOT READ ORIGIN'S PROPERTY " + originProperty.getName() + " OR CANNOT WRITE TARGET'S PROPERTY " + targetProperty.getName();
                 throw new AccessPropertyException(message, cause);
             }
         });
@@ -125,26 +161,55 @@ public class OTABuilder<ORIGIN, TARGET> {
     }
 
     /**
-     * See {@link OTABuilder#build(ORIGIN, TARGET, boolean)}
+     * @see OTABuilder#build(ORIGIN, TARGET, boolean)
      */
     public final TARGET build(ORIGIN origin, TARGET target) {
         return build(origin, target, false);
     }
 
     /**
-     * See {@link OTABuilder#build(ORIGIN, TARGET, boolean)}
+     * @see OTABuilder#build(ORIGIN, TARGET, boolean)
      */
     public final TARGET build(ORIGIN origin) {
         return build(origin, null, false);
     }
 
     /**
-     * See {@link OTABuilder#build(ORIGIN, TARGET, boolean)}
+     * @see OTABuilder#build(ORIGIN, TARGET, boolean)
      */
     public final TARGET build(ORIGIN origin, boolean deepAutomatedBuild) {
         return build(origin, null, deepAutomatedBuild);
     }
 
+    //TODO write documentation
+    private <O, T> Map<PropertyDescriptor, PropertyDescriptor> verifyAndGetMapping(Class<O> origin, Class<T> target) {
+        if (!Objects.equals(origin, target) && !primitivable(origin, target)) {
+            return verifyAndGetMapping(factory.mapperForClasses(origin, target));
+        }
+        return new HashMap<>();
+    }
+
+    //TODO write documentation
+    private <O, T> Map<PropertyDescriptor, PropertyDescriptor> verifyAndGetMapping(OTMapper<O, T> mapper) {
+        final String message = "VERIFIED MAPPING " + mapper.getOriginClass().getName() + " -> " + mapper.getTargetClass().getName() + ": %s";
+        Map<PropertyDescriptor, PropertyDescriptor> mappedProperties;
+        try {
+            mappedProperties = mapper.getMappedProperties();
+        } catch (Exception cause) {
+            logger.severe(String.format(message, "FAILED"));
+            throw cause;
+        }
+        logger.info(String.format(message, "SUCCESS"));
+        mappedProperties.forEach((originProperty, targetProperty) -> {
+            verifyAndGetMapping(originProperty.getPropertyType(), targetProperty.getPropertyType());
+        });
+        return mappedProperties;
+    }
+
+    //TODO write documentation
+    public final void verifyMapping() {
+        verifyAndGetMapping(mapper);
+    }
 
     /**
      * Write origin's property's value to target's property if they are of the same type or if they are attributable
@@ -166,34 +231,26 @@ public class OTABuilder<ORIGIN, TARGET> {
      * @throws CreateInstanceException if this method is trying to create an instance of target's property but
      * default constructor doesn't exists for the property's type
      */
-    @SuppressWarnings("unchecked, rawtypes")
-    private void writeProperty(ORIGIN origin, PropertyDescriptor originProperty, TARGET target,
-                               PropertyDescriptor targetProperty, boolean deepAutomatedBuild)
-            throws IllegalAccessException, InvocationTargetException {
-
-        Object value = originProperty.getReadMethod().invoke(origin);
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    private void writeProperty(ORIGIN origin, PropertyDescriptor originProperty, TARGET target, PropertyDescriptor targetProperty, boolean deepAutomatedBuild) throws IllegalAccessException, InvocationTargetException {
+        Object value = origin != null ? originProperty.getReadMethod().invoke(origin) : null;
         if (value != null) {
             Class<?> targetPropertyType = targetProperty.getPropertyType();
             Class<?> originPropertyType = originProperty.getPropertyType();
             if (Objects.equals(originPropertyType, targetPropertyType) || primitivable(originPropertyType, targetPropertyType)) {
                 targetProperty.getWriteMethod().invoke(target, value);
-            } else if (deepAutomatedBuild || OTMapperRepository.exists(originPropertyType, targetPropertyType)) {
-                OTABuilder builder = instance(originPropertyType, targetPropertyType);
+            } else if (deepAutomatedBuild || (repository != null && repository.exists(originPropertyType, targetPropertyType))) {
+                OTABuilder builder = instance(repository, originPropertyType, targetPropertyType);
                 value = builder.build(value, createInstance(targetPropertyType), deepAutomatedBuild);
                 targetProperty.getWriteMethod().invoke(target, value);
             }
+            if (targetProperty.getReadMethod() != null) {
+                Object targetValue = targetProperty.getReadMethod().invoke(target);
+                if (targetValue == null || (primitivable(originPropertyType, targetPropertyType) && !value.equals(targetValue))) {
+                    throw new OTException(targetProperty.getName() + ": WRITE PROPERTY FAILED! ORIGIN VALUE = " + value + " - TARGET VALUE = " + targetValue);
+                }
+            }
         }
-    }
-
-    /**
-     *
-     * @param origin origin type
-     * @param target target type
-     * @return if the origin is primitive and the target is a wrapper for the same primitive or viceversa
-     */
-    private boolean primitivable(Class<?> origin, Class<?> target) {
-        return (origin.isPrimitive() && Objects.equals(origin, SIMPLE_WRAPPER_TYPES.get(target)))
-                || (target.isPrimitive() && Objects.equals(target, SIMPLE_WRAPPER_TYPES.get(origin)));
     }
 
     /**
@@ -205,6 +262,16 @@ public class OTABuilder<ORIGIN, TARGET> {
         } catch (ReflectiveOperationException cause) {
             throw new CreateInstanceException("CANNOT CREATE INSTANCE OF " + type.getName(), cause);
         }
+    }
+
+    /**
+     *
+     * @param origin origin type
+     * @param target target type
+     * @return if the origin is primitive and the target is a wrapper for the same primitive or viceversa
+     */
+    private boolean primitivable(Class<?> origin, Class<?> target) {
+        return (origin.isPrimitive() && Objects.equals(origin, SIMPLE_WRAPPER_TYPES.get(target))) || (target.isPrimitive() && Objects.equals(target, SIMPLE_WRAPPER_TYPES.get(origin)));
     }
 
 }
--- src/main/java/com/opymi/otamap/util/OTMapperBuilder.java
+++ src/main/java/com/opymi/otamap/util/OTMapperBuilder.java
@@ -26,12 +26,9 @@ package com.opymi.otamap.util;
 
 
 import com.opymi.otamap.exception.CustomizeMappingException;
+import com.opymi.otamap.exception.OTException;
 
-import java.beans.BeanInfo;
-import java.beans.IntrospectionException;
-import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
-import java.lang.reflect.Field;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -79,18 +76,42 @@ public class OTMapperBuilder<ORIGIN, TARGET> {
      * @return current instance of {@link OTMapperBuilder}
      */
     public OTMapperBuilder<ORIGIN, TARGET> customize(String originField, String targetField) {
-        mapper.addCutomNameMapping(targetField, originField);
+        mapper.addCutomNameMapping(originField, targetField);
+        return this;
+    }
+
+    /**
+     * Customize the mapping for fields with different names
+     * @param originTargetFieldMap Map that contains originField as key and targetField as value
+     *
+     * @return current instance of {@link OTMapperBuilder}
+     */
+    public OTMapperBuilder<ORIGIN, TARGET> customize(Map<String, String> originTargetFieldMap) {
+        originTargetFieldMap.forEach((key, value) -> mapper.addCutomNameMapping(key, value));
         return this;
     }
 
     /**
      * Exclude target's field from the mapping
      * @param field target's field name
+     * @param force if true doesn't check if the field exists
      *
      * @return current instance of {@link OTMapperBuilder}
      */
-    public OTMapperBuilder<ORIGIN, TARGET> excludeTargetField(String field) {
-        mapper.excludeTargetField(field);
+    public OTMapperBuilder<ORIGIN, TARGET> excludeField(String field, boolean force) {
+        mapper.excludeField(field, force);
+        return this;
+    }
+
+    //TODO write documentation
+    public OTMapperBuilder<ORIGIN, TARGET> excludeField(String field) {
+        excludeField(field, false);
+        return this;
+    }
+
+    //TODO write documentation
+    public OTMapperBuilder<ORIGIN, TARGET> excludeAllFields() {
+        mapper.excludeAllFields();
         return this;
     }
 
@@ -101,7 +122,6 @@ public class OTMapperBuilder<ORIGIN, TARGET> {
         return mapper;
     }
 
-
     /**
      * Implementation of {@link OTMapper}
      *
@@ -109,13 +129,19 @@ public class OTMapperBuilder<ORIGIN, TARGET> {
      * @param <TARGET>
      */
     private static class OTMapperImpl<ORIGIN, TARGET> implements OTMapper<ORIGIN, TARGET> {
+        private static final List<Class<?>> UNSUPPORTED_TYPES;
+        static {
+            UNSUPPORTED_TYPES = new ArrayList<>();
+            UNSUPPORTED_TYPES.add(Collection.class);
+            UNSUPPORTED_TYPES.add(Map.class);
+        }
+
         private final Class<ORIGIN> origin;
         private final Class<TARGET> target;
         private final Set<String> orginDeclaredProperties;
         private final Set<String> targetDeclaredProperties;
-
-        private final Map<String, String> customNameMapping = new HashMap<>();
-        private final Set<String> excludedFields = new HashSet<>();
+        private final Map<String, String> customNameMapping;
+        private final Set<String> excludedFields;
         private OTCustomMapperOperation<ORIGIN, TARGET> OTCustomMapperOperation;
 
         private OTMapperImpl(Class<ORIGIN> origin, Class<TARGET> target) {
@@ -126,29 +152,36 @@ public class OTMapperBuilder<ORIGIN, TARGET> {
             this.target = target;
             this.orginDeclaredProperties = getDeclaredFields(origin);
             this.targetDeclaredProperties = getDeclaredFields(target);
+            this.excludedFields = new HashSet<>();
+            this.customNameMapping = new HashMap<>();
         }
-
 
         @Override
         public Map<PropertyDescriptor, PropertyDescriptor> getMappedProperties() {
-            Map<String, PropertyDescriptor> originProperties = retrievePropertyDescriptors(origin).stream()
-                    .collect(Collectors.toMap(PropertyDescriptor::getName, p -> p));
+            final String errorMessage = "%s: %s %s! EXCLUDE FIELD AND ADD CUSTOM MAPPING";
 
+            Map<String, PropertyDescriptor> targetProperties = OTFieldsScanner.retrievePropertyDescriptors(target).stream().collect(Collectors.toMap(PropertyDescriptor::getName, p -> p));
             Map<PropertyDescriptor, PropertyDescriptor> mappedProperties = new HashMap<>();
 
-            retrievePropertyDescriptors(target).stream()
-                    .filter(property -> {
-                        String name = property.getName();
-                        return targetDeclaredProperties.contains(name) && !excludedFields.contains(name) && property.getWriteMethod() != null;
-                    })
-                    .forEach(targetProperty -> {
-                        String targetPropertyName = targetProperty.getName();
-                        String originPropertyName = customNameMapping.getOrDefault(targetPropertyName, targetPropertyName);
-                        PropertyDescriptor originProperty = originProperties.get(originPropertyName);
-                        if (originProperty != null && originProperty.getReadMethod() != null) {
-                            mappedProperties.put(originProperty, targetProperty);
-                        }
-                    });
+            OTFieldsScanner.retrievePropertyDescriptors(origin).stream().filter(property -> !excludedFields.contains(property.getName())).forEach(originProperty -> {
+                String originPropertyName = originProperty.getName();
+                if (originProperty.getReadMethod() == null) {
+                    throw new OTException(String.format(errorMessage, origin.getName(), originPropertyName, "READ METHOD NOT FOUND"));
+                }
+
+                String targetPropertyName = customNameMapping.getOrDefault(originPropertyName, originPropertyName);
+                if (!excludedFields.contains(targetPropertyName)) {
+                    PropertyDescriptor targetProperty = targetProperties.get(targetPropertyName);
+                    if (targetProperty == null) {
+                        throw new OTException(String.format(errorMessage, origin.getName(), originPropertyName, "MATCH NOT FOUND! DOESN'T EXIST FIELD WITH SAME NAME IN " + target.getName()));
+                    } else if (targetProperty.getWriteMethod() == null) {
+                        throw new OTException(String.format(errorMessage, target.getName(), targetPropertyName, "WRITE METHOD NOT FOUND"));
+                    } else if (targetProperty.getPropertyType().isArray() || UNSUPPORTED_TYPES.stream().anyMatch(type -> type.isAssignableFrom(targetProperty.getPropertyType()))) {
+                        throw new OTException(String.format(errorMessage, target.getName(), targetPropertyName, "TYPE NOT SUPPORTED " + targetProperty.getPropertyType().getSimpleName()));
+                    }
+                    mappedProperties.put(originProperty, targetProperty);
+                }
+            });
 
             return mappedProperties;
         }
@@ -168,7 +201,6 @@ public class OTMapperBuilder<ORIGIN, TARGET> {
             return target;
         }
 
-
         /**
          * Add the custom behavior
          * @param customMapperOperation implementation of {@link OTCustomMapperOperation} that defines custom operations
@@ -183,46 +215,36 @@ public class OTMapperBuilder<ORIGIN, TARGET> {
          * @param originField origin's field name
          * @param targetField target's field name
          */
-        private void addCutomNameMapping(String targetField, String originField) {
+        private void addCutomNameMapping(String originField, String targetField) {
             existField(targetDeclaredProperties, targetField);
             existField(orginDeclaredProperties, originField);
-            customNameMapping.put(targetField, originField);
+            customNameMapping.put(originField, targetField);
         }
 
         /**
-         * Exclude target's field from the mapping
-         * @param field target's field name
+         * Exclude field from the mapping
+         * @param field field's name
          */
-        private void excludeTargetField(String field) {
-            existField(targetDeclaredProperties, field);
+        private void excludeField(String field, boolean force) {
+            if (!force) {
+                existField(field);
+            }
             excludedFields.add(field);
         }
 
         /**
-         * @return list of {@link PropertyDescriptor} of the type {@param type}
+         * Exclude field from the mapping
          */
-        private List<PropertyDescriptor> retrievePropertyDescriptors(Class<?> type) {
-            BeanInfo beanInfo;
-            try {
-                beanInfo = Introspector.getBeanInfo(type);
-            } catch (IntrospectionException exception) {
-                throw new RuntimeException("CANNOT RETRIEVE BEAN INFO", exception);
-            }
-
-            return Arrays.asList(beanInfo.getPropertyDescriptors());
+        private void excludeAllFields() {
+            excludedFields.addAll(orginDeclaredProperties);
+            excludedFields.addAll(targetDeclaredProperties);
         }
 
         /**
          * @return declared fields of {@param clazz}
          */
         private Set<String> getDeclaredFields(Class<?> clazz) {
-            Set<String> fields = new HashSet<>();
-            Class<?> currentClass = clazz;
-            while(currentClass != Object.class) {
-                Arrays.stream(currentClass.getDeclaredFields()).map(Field::getName).forEach(fields::add);
-                currentClass = currentClass.getSuperclass();
-            }
-            return fields;
+            return new HashSet<>(OTFieldsScanner.findFields(clazz));
         }
 
         /**
@@ -230,6 +252,12 @@ public class OTMapperBuilder<ORIGIN, TARGET> {
          */
         private void existField(Set<String> properties, String toCheck) {
             if (!properties.contains(toCheck)) {
+                throw new CustomizeMappingException("THE FIELD " + toCheck + " DOES NOT EXIST");
+            }
+        }
+
+        private void existField(String toCheck) {
+            if (!orginDeclaredProperties.contains(toCheck) && !targetDeclaredProperties.contains(toCheck)) {
                 throw new CustomizeMappingException("THE FIELD " + toCheck + " DOES NOT EXIST");
             }
         }
