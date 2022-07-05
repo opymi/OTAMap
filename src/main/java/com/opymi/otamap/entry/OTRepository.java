77b34057089d8bd05bbc08f52049cc1688b9df16
Antonino Verde <40131319+opymi@users.noreply.github.com>
2022-07-05 19:13:24 +0200

    Added repository interface

---
+++ src/main/java/com/opymi/otamap/entry/OTRepository.java
@@ -0,0 +1,71 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021. Antonino Verde
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.opymi.otamap.entry;
+
+/**
+ * Repository of {@link OTTransmuter} defined by user
+ *
+ * @author Antonino Verde
+ * @since 2.0
+ */
+public interface OTRepository {
+
+    /**
+     * @param origin origin's type
+     * @param target target's type
+     *
+     * @return true if exists transmuter for types
+     */
+    <ORIGIN, TARGET> boolean exists(Class<ORIGIN> origin, Class<TARGET> target);
+
+    /**
+     * store {@param transmuter}
+     */
+    <ORIGIN, TARGET, TRANSMUTER extends OTTransmuter<ORIGIN, TARGET>> void store(TRANSMUTER transmuter);
+
+    /**
+     * store transmuter by {@param definer}
+     */
+    <ORIGIN, TARGET, TRANSMUTER extends OTTransmuter<ORIGIN, TARGET>> void store(OTCustomTransmuterDefiner<ORIGIN, TARGET, TRANSMUTER> definer);
+
+    /**
+     * @param origin origin's type
+     * @param target target's type
+     *
+     * @return transmuter for types
+     */
+    <ORIGIN, TARGET, TRANSMUTER extends OTTransmuter<ORIGIN, TARGET>> TRANSMUTER get(Class<ORIGIN> origin, Class<TARGET> target);
+
+    /**
+     * remove transmuter by types
+     */
+    <ORIGIN, TARGET> void remove(Class<ORIGIN> origin, Class<TARGET> target);
+
+    /**
+     * remove transmuter by {@param definer}
+     */
+    <ORIGIN, TARGET, TRANSMUTER extends OTTransmuter<ORIGIN, TARGET>> void remove(OTCustomTransmuterDefiner<ORIGIN, TARGET, TRANSMUTER> definer);
+
+}
