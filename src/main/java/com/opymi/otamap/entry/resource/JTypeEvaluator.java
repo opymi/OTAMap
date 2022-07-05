75f0ed40cde7935877a3de0300a421e65ff22113
Antonino Verde <40131319+opymi@users.noreply.github.com>
2022-07-05 19:10:41 +0200

    Added java base type evaluator

---
+++ src/main/java/com/opymi/otamap/entry/resource/JTypeEvaluator.java
@@ -0,0 +1,46 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021. Antonino Verde
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.opymi.otamap.entry.resource;
+
+import com.opymi.otamap.annotation.OTAResource;
+
+/**
+ * Utility to evaluate java base type
+ *
+ * @author Antonino Verde
+ * @since 2.0
+ */
+@OTAResource
+public interface JTypeEvaluator {
+
+    /**
+     * @param origin origin type
+     * @param target target type
+     *
+     * @return true if the origin is primitive and the target is a wrapper for the same primitive or viceversa
+     */
+    boolean isPrimitivable(Class<?> origin, Class<?> target);
+
+}
